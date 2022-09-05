5a19da3d464965b9989afb94125a1226d16d972e
Kirill <kirilltcarev1999@gmail.com>
2022-09-05 14:18:57 +0300

    решение задачи 2

---
+++ Zadacha02/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("введите число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+int count = 1;
+if(num1<=99)
+{
+    Console.WriteLine("третьей цифры нет");
+
+}
+else
+{
+
+while(num1>1000)
+{
+    num1 = num1 / 10;
+    count++;
+    
+}
+num1 = num1 % 10;
+Console.WriteLine($"третье число это - {num1}");
+}
