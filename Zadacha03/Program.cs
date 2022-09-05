dab182a90350c2fc4c542fdcfad7f35ec9b1dc45
Kirill <kirilltcarev1999@gmail.com>
2022-09-05 14:21:58 +0300

    решение задачи 3

---
+++ Zadacha03/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("введите число, обозначающее номер дня недели");
+int num1 = Convert.ToInt32(Console.ReadLine());
+    
+if(num1<=5 && num1>0)
+{
+    Console.WriteLine("нет");
+}
+else if(num1>5  && num1<=7 )
+{
+    Console.WriteLine("да");
+}
