4b6a0fe74693ad7ee1407981f507a83886039587
Kirill <kirilltcarev1999@gmail.com>
2022-09-05 14:15:21 +0300

    решение первой задачи

---
+++ Zadacha01/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("введите трёхзначное число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+if(num1>=100 && num1<1000)
+{
+num1 = num1 /10 ;
+
+num1 = num1 % 10 ;
+
+Console.WriteLine(num1);
+}
+else
+{
+Console.WriteLine("введите трёхзначное число");
+}
